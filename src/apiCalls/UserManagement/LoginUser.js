808c5c0f3cf0d2badd34050409d5cbd2b8d15f21
amsamuels <amsamuels21@gmail.com>
2023-03-09 15:44:43 +0000

    added error handling for all status code the server sends back added comments to login component and api request file

--- src/apiCalls/UserManagement/LoginUser.js
+++ src/apiCalls/UserManagement/LoginUser.js
@@ -1,33 +1,45 @@
 import StoreData from '../StoreData';
 import { API_URL } from '@env';
 
+// This function is used to login a user
 const LoginUser = async (
   user,
   setLoggedIn,
   setIcorrectLogin,
-  setLoginError
+  setLoginError,
+  setServerError
 ) => {
   try {
     const res = await fetch(`${API_URL}login`, {
-      method: 'POST',
+      // Make a request to the login endpoint
+      method: 'POST', // Use the POST method
       headers: {
+        // Set the headers
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(user), // Convert the user object to a json string
     });
-    const resJson = await res.json();
+    const resJson = await res.json(); // Get the response as json
     if (res?.status === 200) {
-      console.log('login: Successfully logged user in');
-      StoreData('token', resJson.token.toString());
-      StoreData('id', resJson.id.toString());
-      setLoggedIn(true);
+      // 200 is the status code for a successful request
+      console.log('login: Successfully logged user in'); // Log the success
+      StoreData('token', resJson.token.toString()); // Store the token in async storage
+      StoreData('id', resJson.id.toString()); // Store the id in async storage
+      setLoggedIn(true); // Set the logged in state to true
     } else if (res?.status === 400) {
-      setIcorrectLogin(true);
+      // 400 is the status code for a bad request
+      console.log('Incorrect login details');
+      setIcorrectLogin(true); // Set the incorrect login to true
+    } else if (res?.status === 500) {
+      // 500 is the status code for a server error
+      console.log('There was a problem with our server');
+      setServerError(true);
     }
   } catch (error) {
-    setLoginError(true);
-    console.error('Failed to login user:', error);
+    // If there is an error with the request
+    setLoginError(true); // Set the login error to true
+    console.error('Failed to login user:', error); // Log the error
     throw error;
   }
 };
--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -9,51 +9,75 @@ import { ShowToast } from '../../apiCalls';
 import { ROUTES } from '../../constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// This is the schema for the form
 const FormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8, { message: 'Password needs to be 8 characters' }),
+  email: z.string().email(), // The email field needs to be a string and a valid email
+  password: z.string().min(8, { message: 'Password needs to be 8 characters' }), // The password field needs to be a string and at least 8 characters long
 });
 
 const Login = (props) => {
+  // Destructure the navigation prop
   const { navigation } = props;
   const { ...methods } = useForm({
-    resolver: zodResolver(FormSchema),
+    // Use the useForm hook
+    resolver: zodResolver(FormSchema), // Use the zod resolver
   });
 
   const [loggedIn, setLoggedIn] = useState(false);
   const [icorrectLogin, setIcorrectLogin] = useState(false);
   const [loginError, setLoginError] = useState(false);
+  const [serverError, setServerError] = useState(false);
 
+  // This function is used to check if a user is logged in
   const checkToken = async () => {
     const token = await AsyncStorage.getItem('@token');
     if (token) {
-      navigation.navigate(ROUTES.HOME);
+      // If there is a token in async storage
+      navigation.navigate(ROUTES.HOME); // Navigate to the home screen
+      ShowToast('success', 'Welcome back'); // Show a welcome back toast
     } else {
+      // If there is no token in async storage
       console.log('No token found');
-      navigation.navigate(ROUTES.LOGIN);
+      navigation.navigate(ROUTES.LOGIN); // Navigate to the login screen
     }
   };
 
+  // This function is used to login a user
   const onSubmit = async (data) => {
     try {
       setIcorrectLogin(false);
       setLoginError(false);
-      await LoginUser(data, setLoggedIn, setIcorrectLogin, setLoginError);
+      setServerError(false);
+      await LoginUser(
+        data,
+        setLoggedIn,
+        setIcorrectLogin,
+        setLoginError,
+        setServerError
+      );
     } catch (error) {
       console.error('Failed to Login user:', error);
     }
   };
+  // This useEffect is used to check if a user is logged in
   useEffect(() => {
-    checkToken();
+    checkToken(); // Check if a user is logged in
     if (loggedIn) {
+      // If the user is logged in
       ShowToast('success', 'Login successful', 'Welcome back');
-      navigation.navigate(ROUTES.HOME);
+      navigation.navigate(ROUTES.HOME); // Navigate to the home screen
     }
     if (icorrectLogin) {
-      ShowToast('error', 'Oops, Incorrect login details. Try again.');
+      // If the user is not logged in
+      ShowToast('error', 'Oops, Incorrect login details. Try again.'); // Show a toast
     }
     if (loginError) {
-      ShowToast('error', 'Oops, Error logging in. Try again.');
+      // If there is an error logging in
+      ShowToast('error', 'Oops, Error logging in. Try again.'); // Show a toast
+    }
+    if (serverError) {
+      // If there is a server error
+      ShowToast('error', 'Sorry ServerError logging in. Try again.'); // Show a toast
     }
   }, [loggedIn, icorrectLogin, loginError]);
 
